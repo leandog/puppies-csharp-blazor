373688a8f8632b6c7cd573c1cb76c2e77984bb67
Bryan Beecham <bryan_beecham@hotmail.com>
2025-04-28 10:29:16 -0400

    Form created but not validating properly

--- Puppies/PuppyOwner.cs
+++ Puppies/PuppyOwner.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Puppies;
 
 public class PuppyOwner
 {
+    [Required]
     public string Name { get; set; }
-    public string Address { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string PayType { get; set; }
 }   
